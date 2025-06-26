76b2037879f049bff294a609289f516e3765b10f
Ian Campbell <iwc3@bu.edu>
2025-06-25 23:04:32 -0400

    Adding finexam and testfile

---
+++ finexam/00/MySolution/testfile.py
@@ -0,0 +1,64 @@
+#factorial function
+
+def fun104(arg0):
+    tmp109 = 0
+    tmp110 = arg0 <= tmp109
+
+    if tmp110:
+        tmp111 = 1
+        tmp116 = tmp111
+    else:
+        tmp112 = 1
+        tmp113 = arg0 - tmp112
+        tmp114 = fun104(tmp113)
+        tmp115 = arg0 * tmp114
+        tmp116 = tmp115
+
+    return tmp116
+
+
+
+# isPrime function 
+
+def fun105(arg0):
+    tmp117 = 1
+    tmp118 = arg0 <= tmp117
+
+    if tmp118:
+        tmp119 = 0
+        tmp133 = tmp119
+    else:
+
+        def fun106(arg0):
+            tmp120 = 1
+            tmp121 = arg0 - tmp120
+            tmp122 = arg0 <= tmp121
+
+            if tmp122:
+                tmp123 = arg0 % arg0
+
+                if tmp123:
+                    tmp124 = 1
+                    tmp125 = arg0 + tmp124
+                    tmp126 = fun106(tmp125)
+                    tmp128 = tmp126
+                else:
+                    tmp127 = 0
+                    tmp128 = tmp127
+
+                tmp130 = tmp128
+            else:
+                tmp129 = 1
+                tmp130 = tmp129
+
+            return tmp130
+
+        tmp131 = 2
+        tmp132 = fun106(tmp131)
+        tmp133 = tmp132
+
+    return tmp133
+
+print(fun104(5))
+print(fun105(7))
+print
